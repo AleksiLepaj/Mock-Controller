8647860ed9745352b90b5439a91452e7daea118e
Alexis <2175497@cegepmontpetit.ca>
2024-12-06 09:49:57 -0500

    FCT Tests Unitaires
    Complétion des tests unitaires avec mock

--- mockTests2/Controllers/CatsControllerTests.cs
+++ mockTests2/Controllers/CatsControllerTests.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using mock.depart.Controllers;
+using mock.depart.Models;
+using mock.depart.Services;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +16,104 @@ namespace mock.depart.Controllers.Tests
     public class CatsControllerTests
     {
         [TestMethod()]
-        public void DeleteCatTest()
+        public void Delete_CatNotFound()
         {
-            Assert.Fail();
+            Mock<CatsService> serviceMock = new Mock<CatsService>();
+            Mock<CatsController> controller = new Mock<CatsController>(serviceMock.Object) { CallBase = true };
+
+            serviceMock.Setup(s => s.Get(It.IsAny<int>())).Returns(value: null);
+            controller.Setup(c => c.UserId).Returns("1");
+
+            var actionResult = controller.Object.DeleteCat(0);
+            var result = actionResult.Result as NotFoundResult;
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void Delete_WrongOwner()
+        {
+            Mock<CatsService> serviceMock = new Mock<CatsService>();
+            Mock<CatsController> controller = new Mock<CatsController>(serviceMock.Object) { CallBase = true };
+
+            CatOwner co = new CatOwner()
+            {
+                Id = "11111"
+            };
+            Cat c = new Cat()
+            {
+                Id = 1,
+                Name = "Meow",
+                CatOwner = co,
+                CuteLevel = Cuteness.Amazing
+            };
+
+            serviceMock.Setup(s => s.Get(It.IsAny<int>())).Returns(c);
+            controller.Setup(c => c.UserId).Returns("1");
+
+            var actionResult = controller.Object.DeleteCat(0);
+            var result = actionResult.Result as BadRequestObjectResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Cat is not yours", result.Value);
+        }
+
+        [TestMethod()]
+        public void TooCute()
+        {
+            Mock<CatsService> serviceMock = new Mock<CatsService>();
+            Mock<CatsController> controller = new Mock<CatsController>(serviceMock.Object) { CallBase = true };
+
+            CatOwner co = new CatOwner()
+            {
+                Id = "11111"
+            };
+            Cat c = new Cat()
+            {
+                Id = 1,
+                Name = "Meow",
+                CatOwner = co,
+                CuteLevel = Cuteness.Amazing
+            };
+
+            serviceMock.Setup(s => s.Get(It.IsAny<int>())).Returns(c);
+            controller.Setup(c => c.UserId).Returns("11111");
+
+            var actionResult = controller.Object.DeleteCat(0);
+            var result = actionResult.Result as BadRequestObjectResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Cat is too cute", result.Value);
+        }
+
+        [TestMethod()]
+        public void Delete_Ok()
+        {
+            Mock<CatsService> serviceMock = new Mock<CatsService>();
+            Mock<CatsController> controller = new Mock<CatsController>(serviceMock.Object) { CallBase = true };
+
+            CatOwner co = new CatOwner()
+            {
+                Id = "11111"
+            };
+            Cat c = new Cat()
+            {
+                Id = 1,
+                Name = "Meow",
+                CatOwner = co,
+                CuteLevel = Cuteness.BarelyOk
+            };
+
+            serviceMock.Setup(s => s.Get(It.IsAny<int>())).Returns(c);
+            serviceMock.Setup(s => s.Delete(It.IsAny<int>())).Returns(c);
+            controller.Setup(c => c.UserId).Returns("11111");
+
+            var actionResult = controller.Object.DeleteCat(0);
+            var result = actionResult.Result as OkObjectResult;
+
+            Assert.IsNotNull(result);
+
+            Cat? catResult = (Cat?)result!.Value;
+            Assert.AreEqual(c.Id, catResult!.Id);
         }
     }
 }
